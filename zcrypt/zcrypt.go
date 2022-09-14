c3ca23dfe3fd7ba27c6e324141ff0eb6e217147b
whatwewant <tobewhatwewant@outlook.com>
2022-09-14 18:49:31 +0800

    feat: add zcrypt like bcrypt

---
+++ zcrypt/zcrypt.go
@@ -0,0 +1,26 @@
+// inspired by bcrypt: https://zhuanlan.zhihu.com/p/400196101
+// zcrypt algorithm: timestamp.random_id.hmacSha256(password, timestamp)
+package zcrypt
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/go-zoox/crypto/hmac"
+	"github.com/go-zoox/random"
+)
+
+// Generate a hash from password
+func Generate(password string) string {
+	timestamp := fmt.Sprintf("%d", time.Now().UnixMilli())
+	randomID := random.String(10)
+	return fmt.Sprintf("%s.%s.%s", timestamp, randomID, hmac.Sha256(password, timestamp+randomID))
+}
+
+// Compare a hash with password
+func Compare(password, hash string) bool {
+	parts := strings.Split(hash, ".")
+	timestamp, randomID, hash := parts[0], parts[1], parts[2]
+	return hash == hmac.Sha256(password, timestamp+randomID)
+}
